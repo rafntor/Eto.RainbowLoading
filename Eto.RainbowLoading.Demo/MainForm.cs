b05a005d046e26eaa46fa339e72d513f3ea525ac
Tore Fuglestad <tore@fuglestad.no>
2022-02-01 19:46:52 +0100

    make skiasharp-based version

--- Eto.RainbowLoading.Demo/MainForm.cs
+++ Eto.RainbowLoading.Demo/MainForm.cs
@@ -9,15 +9,23 @@ namespace Eto.RainbowLoading.Demo
 	public partial class MainForm : Form
 	{
 		private readonly Random rand = new Random();
+		private CheckBox skia = new CheckBox() { Text = "Use Skia", Font = Fonts.Sans(20) };
 		public MainForm()
 		{
 			Title = "My Eto Form";
-			MinimumSize = new Size(200, 200);
-			Size = new Size(800, 600);
+			ClientSize = new Size(3, 2) * 300;
 
+			skia.CheckedChanged += (o, e) => { MakeLayout(); };
+
+			MakeLayout();
+		}
+
+		void MakeLayout()
+		{ 
 			var cells = GetVariationCells();
 
 			var layout1 = new TableLayout() { Spacing = Size.Empty + 4 };
+			layout1.Rows.Add(new TableRow(new TableCell(skia)));
 			layout1.Rows.Add(new TableRow(cells[0], cells[1], cells[2]) { ScaleHeight = true });
 			layout1.Rows.Add(new TableRow(cells[3], cells[4], cells[5]) { ScaleHeight = true });
 
@@ -31,29 +39,58 @@ namespace Eto.RainbowLoading.Demo
 		{
 			List<TableLayout> list = new List<TableLayout>();
 
-			list.Add(new TableLayout(
-				new Label { Text = "Defaults" },
-				new RainbowLoading()));
+			if (skia.Checked ?? false)
+			{
+				list.Add(new TableLayout(
+					new Label { Text = "Defaults" },
+					new Skia.RainbowLoading()));
 
-			list.Add(new TableLayout(
-				new Label { Text = "Progress Duration = 4000 millis" },
-				new RainbowLoading { ProgressDuration = TimeSpan.FromMilliseconds(4000) }));
+				list.Add(new TableLayout(
+					new Label { Text = "Progress Duration = 4000 millis" },
+					new Skia.RainbowLoading { ProgressDuration = TimeSpan.FromMilliseconds(4000) }));
 
-			list.Add(new TableLayout(
-				new Label { Text = "Progress Duration = 1000 millis" },
-				new RainbowLoading { ProgressDuration = TimeSpan.FromMilliseconds(1000) }));
+				list.Add(new TableLayout(
+					new Label { Text = "Progress Duration = 1000 millis" },
+					new Skia.RainbowLoading { ProgressDuration = TimeSpan.FromMilliseconds(1000) }));
 
-			list.Add(new TableLayout(
-				new Label { Text = "Pastel Colours" },
-				new RainbowLoading { ProgressColors = GetListOfRandomPastelColors(28) }));
+				list.Add(new TableLayout(
+					new Label { Text = "Custom Shadow Color" },
+					new Skia.RainbowLoading { ShadowColor = new Color(GetRandomPastelColor(), 0.9f) }));
 
-			list.Add(new TableLayout(
-				new Label { Text = "Custom Background Color" },
-				new RainbowLoading { BackgroundColor = new Color(GetRandomPastelColor(), 0.5f) }));
+				list.Add(new TableLayout(
+					new Label { Text = "Custom Background Color" },
+					new Skia.RainbowLoading { BackgroundColor = new Color(GetRandomPastelColor(), 0.5f) }));
 
-			list.Add(new TableLayout(
-				new Label { Text = "Custom Control Color" },
-				new RainbowLoading { ControlColor = GetRandomPastelColor() }));
+				list.Add(new TableLayout(
+					new Label { Text = "Custom Control Color" },
+					new Skia.RainbowLoading { ControlColor = GetRandomPastelColor() }));
+			}
+			else
+			{
+				list.Add(new TableLayout(
+					new Label { Text = "Defaults" },
+					new RainbowLoading()));
+
+				list.Add(new TableLayout(
+					new Label { Text = "Progress Duration = 4000 millis" },
+					new RainbowLoading { ProgressDuration = TimeSpan.FromMilliseconds(4000) }));
+
+				list.Add(new TableLayout(
+					new Label { Text = "Progress Duration = 1000 millis" },
+					new RainbowLoading { ProgressDuration = TimeSpan.FromMilliseconds(1000) }));
+
+				list.Add(new TableLayout(
+					new Label { Text = "Pastel Colors" },
+					new RainbowLoading { ProgressColors = GetListOfRandomPastelColors(28) }));
+
+				list.Add(new TableLayout(
+					new Label { Text = "Custom Background Color" },
+					new RainbowLoading { BackgroundColor = new Color(GetRandomPastelColor(), 0.5f) }));
+
+				list.Add(new TableLayout(
+					new Label { Text = "Custom Control Color" },
+					new RainbowLoading { ControlColor = GetRandomPastelColor() }));
+			}
 
 			return list;
 		}
